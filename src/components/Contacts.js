dab09afd8a0e9ea49f3bfe44bb728e5deb689082
Yauheni5 <yauheni5@github.com>
2022-11-28 00:26:29 +0300

    feat: add content to section contacts and fix source css

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,1 +1,21 @@
-export default function Contacts() {}
+import { NavLink } from 'react-router-dom';
+import arrowRight from '../images/arrowRight.png';
+
+export default function Contacts() {
+  return (
+    <section className="contacts">
+      <div className='contacts__info'>
+        <h2 className="contacts__title">Останемся на связи?</h2>
+        <p className="contacts__text">Подпишитесь на нас в соцсетях, у нас там много интересного.</p>
+      </div>
+      <div className="contacts__animation">
+        <img src={arrowRight} alt="Стрелка направление"/>
+      </div>
+      <nav className='contacts__nav'>
+        <NavLink to="#" className='contacts__link contacts__link_journal'>Журнал</NavLink>
+        <NavLink to="#" className='contacts__link contacts__link_vk'>VK</NavLink>
+        <NavLink to="#" className='contacts__link contacts__link_telegram'>telegram</NavLink>
+      </nav>
+    </section>
+  )
+}
