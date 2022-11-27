309fc184a89306ede0343813a09f1511b63769a8
Maria Arkhipova <marias1403@yandex.ru>
2022-11-28 00:12:56 +0900

    css-styles for main added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,25 @@
-export default function Main() {}
+import MainInfo from "./MainInfo";
+import Description from "./Description";
+import Faculties from "./Faculties";
+import Roadmap from "./Roadmap";
+import Chatbot from "./Chatbot";
+import Vakancies from "./Vakancies";
+import Faq from "./Faq";
+import Comments from "./Comments";
+import Contacts from "./Contacts";
+
+export default function Main() {
+    return (
+        <main className="content page__content">
+            <MainInfo />
+            <Description />
+            <Faculties />
+            <Roadmap />
+            <Chatbot />
+            <Vakancies />
+            <Faq />
+            <Comments />
+            <Contacts />
+        </main>
+    )
+}
