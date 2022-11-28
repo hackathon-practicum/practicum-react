b579f5ffb3d80b4bbc8858015ea6fc94d741b1fd
Maria Arkhipova <marias1403@yandex.ru>
2022-11-29 00:17:25 +0900

    feat: faculties

--- src/components/Faculties.js
+++ src/components/Faculties.js
@@ -1,1 +1,51 @@
-export default function Faculties() {}
+import React from "react";
+import FacultiesCard from "./FacultiesCard";
+import {FreeMode, Navigation} from "swiper";
+import {Swiper, SwiperSlide} from "swiper/react";
+import DescriptionCard from "./DescriptionCard";
+
+export default function Faculties() {
+    return (
+        <section className="faculties section content__section">
+            <h2 className="section__title">Чему мы учим</h2>
+            <div className="faculties__cards-container">
+                <Swiper
+                    slidesPerView={"auto"}
+                    spaceBetween={30}
+                    freeMode={true}
+                    modules={[FreeMode, Navigation]}
+                    navigation={false}
+                    loop={false}
+                    className="my-swiper faculties__swiper"
+                >
+                    <SwiperSlide>
+                        <FacultiesCard
+                            subtitle={'Программирование'}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <FacultiesCard
+                            subtitle={'Дизайн'}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <FacultiesCard
+                            subtitle={'Маркетинг'}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <FacultiesCard
+                            subtitle={'Менеджмент'}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <FacultiesCard
+                            subtitle={'Аналитика'}
+                        />
+                    </SwiperSlide>
+                </Swiper>
+
+            </div>
+        </section>
+    )
+}
