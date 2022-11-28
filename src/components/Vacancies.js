6a0df7469746e7a9867a2900415602ed00ac3027
Yauheni5 <yauheni5@github.com>
2022-11-28 10:54:59 +0300

    feat: add section vacancies

--- src/components/Vacancies.js
+++ src/components/Vacancies.js
@@ -1,1 +1,17 @@
-export default function Vacancies() {}
+import { Link } from "react-router-dom";
+
+export default function Vacancies() {
+  return (
+    <section className="section vacancies">
+      <h2 className="section__title">Открытые предложения</h2>
+      <div className="vacancies__toggle">
+        <Link to="#" className="vacancies__link vacancies__link_active">Для наставников</Link>
+        <label className="vacancies__switch">
+          <input type="checkbox" className="vacancies__checkbox"/>
+          <span className="vacancies__slider" />
+        </label>
+        <Link to="#" className="vacancies__link">Для ревьюверов</Link>
+      </div>
+    </section>
+  )
+}
