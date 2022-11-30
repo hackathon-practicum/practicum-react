278ba3807787d3a733cffd2bc3388352194a66af
Maria Arkhipova <marias1403@yandex.ru>
2022-11-30 22:53:28 +0900

    feat: flip-cards

--- src/components/FacultiesCard.js
+++ src/components/FacultiesCard.js
@@ -1,8 +1,20 @@
+import ReactCardFlip from 'react-card-flip';
+import {useState} from "react";
+
 export default function FacultiesCard(props) {
+    const [isFlipped, setIsFlipped] = useState(false);
+
     return (
-        <div className="faculties__card">
-            <h3 className="faculties__subtitle">{props.subtitle}</h3>
-            <img className="faculties__card-image" src={props.image} alt="#" />
-        </div>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+            <div onClick={() => setIsFlipped(true)} className="faculties__card">
+                <h3 className="faculties__subtitle">{props.subtitle}</h3>
+                <img className="faculties__card-image" src={props.image} alt="#" />
+            </div>
+            <div onClick={() => setIsFlipped(false)} className="faculties__card faculties__card_type_back">
+                <p className="section__text">{props.text}</p>
+                <p className="section__text">{props.subtext}</p>
+                <button className="button-cta faculties__button">Стать экспертом</button>
+            </div>
+        </ReactCardFlip>
     )
 }
