7984e20caf41eb62248ce58e8ea9dca2cf029ae4
Maria Arkhipova <marias1403@yandex.ru>
2022-12-01 17:13:41 +0900

    feat: vacancies-cards

--- src/components/Vacancies.js
+++ src/components/Vacancies.js
@@ -1,9 +1,14 @@
 import { NavLink } from "react-router-dom";
-import { cardDataReviewer } from "../utils/constants";
+import {cardDataReviewer, cardDataMentor, MENTOR, REVIEWER} from "../utils/constants";
 import arrowRight from "../images/arrowRight.png";
-
+import React from "react";
 
 export default function Vacancies() {
+  const [category, setCategory] = React.useState(MENTOR);
+  function handleToggleCategory() {
+    setCategory(category === MENTOR ? REVIEWER : MENTOR);
+  }
+
   function createCardsVacancies(data) {
     return data.map((item, i) => (
       <NavLink to="/vacancy" className="card-vacancies" key={i}>
@@ -18,11 +23,11 @@ export default function Vacancies() {
     <section className="section vacancies">
       <h2 className="section__title vacancies__title">Открытые предложения</h2>
       <div className="vacancies__toggle">
-        <NavLink to="#" className="vacancies__link vacancies__link_active">
+        <NavLink to="#" className="vacancies__link">
           Для наставников
         </NavLink>
         <label className="vacancies__switch">
-          <input type="checkbox" className="vacancies__checkbox" />
+          <input onChange={handleToggleCategory} type="checkbox" className="vacancies__checkbox" />
           <span className="vacancies__slider" />
         </label>
         <NavLink to="#" className="vacancies__link">
@@ -69,7 +74,11 @@ export default function Vacancies() {
         </ul>
       </nav>
       <div className="vacancies__cards">
-        {createCardsVacancies(cardDataReviewer)}
+        {
+          category === REVIEWER
+            ? createCardsVacancies(cardDataReviewer)
+            : createCardsVacancies(cardDataMentor)
+        }
       </div>
       <div className="contacts__wrapper vacancies__form">
         <div className="contacts__info">
