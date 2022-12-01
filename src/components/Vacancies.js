607b8c4f9f69fec24aa6682357b58f0b055e9f2f
Maria Arkhipova <marias1403@yandex.ru>
2022-12-01 19:45:27 +0900

    feat: mobile

--- src/components/Vacancies.js
+++ src/components/Vacancies.js
@@ -16,60 +16,62 @@ export default function Vacancies() {
 
   return (
     <section className="section vacancies">
-      <h2 className="section__title vacancies__title">Открытые предложения</h2>
-      <div className="vacancies__toggle">
-        <NavLink to="#" className="vacancies__link vacancies__link_active">
-          Для наставников
-        </NavLink>
-        <label className="vacancies__switch">
-          <input type="checkbox" className="vacancies__checkbox" />
-          <span className="vacancies__slider" />
-        </label>
-        <NavLink to="#" className="vacancies__link">
-          Для ревьюверов
-        </NavLink>
-      </div>
-      <nav className="vacancies__nav">
-        <ul className="vacancies__list">
-          <li>
-            <NavLink
-              to="#"
-              className="vacancies__navlink vacancies__navlink_active">
-              Программирование
-              <sup className="vacancies__counter vacancies__counter_active">
-              0
-            </sup>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="#" className="vacancies__navlink">
-              Аналитика
-              <sup className="vacancies__counter">0</sup>
-            </NavLink>
+      <div className="vacancies__wrapper">
+        <h2 className="section__title vacancies__title">Открытые предложения</h2>
+        <div className="vacancies__toggle">
+          <NavLink to="#" className="vacancies__link vacancies__link_active">
+            Для наставников
+          </NavLink>
+          <label className="vacancies__switch">
+            <input type="checkbox" className="vacancies__checkbox" />
+            <span className="vacancies__slider" />
+          </label>
+          <NavLink to="#" className="vacancies__link">
+            Для ревьюверов
+          </NavLink>
+        </div>
+        <nav className="vacancies__nav">
+          <ul className="vacancies__list">
+            <li>
+              <NavLink
+                  to="#"
+                  className="vacancies__navlink vacancies__navlink_active">
+                Программирование
+                <sup className="vacancies__counter vacancies__counter_active">
+                  0
+                </sup>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="vacancies__navlink">
+                Аналитика
+                <sup className="vacancies__counter">0</sup>
+              </NavLink>
 
-          </li>
-          <li>
-            <NavLink to="#" className="vacancies__navlink">
-              Дизайн
-              <sup className="vacancies__counter">0</sup>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="#" className="vacancies__navlink">
-              Маркетинг
-              <sup className="vacancies__counter">0</sup>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="#" className="vacancies__navlink">
-              Менеджмент
-              <sup className="vacancies__counter">0</sup>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className="vacancies__cards">
-        {createCardsVacancies(cardDataReviewer)}
+            </li>
+            <li>
+              <NavLink to="#" className="vacancies__navlink">
+                Дизайн
+                <sup className="vacancies__counter">0</sup>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="vacancies__navlink">
+                Маркетинг
+                <sup className="vacancies__counter">0</sup>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="vacancies__navlink">
+                Менеджмент
+                <sup className="vacancies__counter">0</sup>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="vacancies__cards">
+          {createCardsVacancies(cardDataReviewer)}
+        </div>
       </div>
       <div className="vacancies__form">
         <div className="contacts__info">
