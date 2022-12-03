1d0969c734d48b50ec877014b5e0d064bc8b8225
Yauheni5 <yauheni5@github.com>
2022-12-03 23:23:19 +0300

    for review

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,5 +1,5 @@
-import {FreeMode, Navigation, Pagination } from "swiper";
-import {Swiper, SwiperSlide} from "swiper/react";
+import { FreeMode, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import React from "react";
 import CommentsCard from "./CommentsCard";
 import commentsPic from "../images/comments_pic.png";
@@ -8,64 +8,75 @@ import commentsPic3 from "../images/comments_pic3.png";
 import commentsPic4 from "../images/comments_pic4.png";
 
 export default function Comments() {
-    return (
-        <section className="comments section content__section" id="description">
-            <h2 className="section__title section__title_type_white">Отзывы команды</h2>
-            <div className="comments__cards-container">
-                <Swiper
-                    slidesPerView={"auto"}
-                    spaceBetween={20}
-                    freeMode={true}
-                    modules={[FreeMode, Navigation, Pagination]}
-                    navigation={true}
-                    pagination={{
-                        enabled: true,
-                        dynamicBullets: true
-                    }}
-                    loop={false}
-                    className="my-swiper"
-                >
-                    <SwiperSlide>
-                        <CommentsCard
-                            image={commentsPic}
-                            class={'comments__card_type_visible'}
-                            title={'Сергей Игнатьев'}
-                            subtitle={'код-ревьюер'}
-                            text={'Выбрал Практикум, \n' +
-                                'чтобы получить новый опыт и иметь дополнительный заработок.'}
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <CommentsCard
-                            image={commentsPic2}
-                            class={'comments__card_type_visible'}
-                            title={'Данил Богданов'}
-                            subtitle={'наставник разработки'}
-                            text={'Выбрал Практикум, чтобы \n' + 'реализовать себя как профессионал и делиться опытом.'}
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <CommentsCard
-                            image={commentsPic3}
-                            class={'comments__card_type_visible'}
-                            title={'Дмитрий Янов'}
-                            subtitle={'наставник разработки'}
-                            text={'Выбрал Практикум, \n' +
-                                'чтобы получить новый опыт и иметь дополнительный заработок.'}
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <CommentsCard
-                            image={commentsPic4}
-                            class={'comments__card_type_visible'}
-                            title={'Анна Симакова'}
-                            subtitle={'дизайн-ревьюер'}
-                            text={'Выбрала Практикум,\n' +
-                                'чтобы делиться знаниями и\n' + 'наслаждаться этим процессом.'}
-                        />
-                    </SwiperSlide>
-                </Swiper>
-            </div>
-        </section>
-    )
+  return (
+    <section className="comments section content__section" id="description">
+      <h2 className="section__title section__title_type_white">
+        Отзывы команды
+      </h2>
+      <div className="comments__cards-container">
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={20}
+          freeMode={true}
+          modules={[FreeMode, Navigation, Pagination]}
+          navigation={true}
+          pagination={{
+            enabled: true,
+            dynamicBullets: true,
+          }}
+          loop={false}
+          className="my-swiper">
+          <SwiperSlide>
+            <CommentsCard
+              image={commentsPic}
+              class={"comments__card_type_visible"}
+              title={"Сергей Игнатьев"}
+              subtitle={"код-ревьюер"}
+              text={
+                "Выбрал Практикум, \n" +
+                "чтобы получить новый опыт и иметь дополнительный заработок."
+              }
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CommentsCard
+              image={commentsPic2}
+              class={"comments__card_type_visible"}
+              title={"Данил Богданов"}
+              subtitle={"наставник разработки"}
+              text={
+                "Выбрал Практикум, чтобы \n" +
+                "реализовать себя как профессионал и делиться опытом."
+              }
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CommentsCard
+              image={commentsPic3}
+              class={"comments__card_type_visible"}
+              title={"Дмитрий Янов"}
+              subtitle={"наставник разработки"}
+              text={
+                "Выбрал Практикум, \n" +
+                "чтобы получить новый опыт и иметь дополнительный заработок."
+              }
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CommentsCard
+              image={commentsPic4}
+              class={"comments__card_type_visible"}
+              title={"Анна Симакова"}
+              subtitle={"дизайн-ревьюер"}
+              text={
+                "Выбрала Практикум,\n" +
+                "чтобы делиться знаниями и\n" +
+                "наслаждаться этим процессом."
+              }
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import './index.css';
-import App from '../src/components/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import "./index.css";
+import App from "../src/components/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-    document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
