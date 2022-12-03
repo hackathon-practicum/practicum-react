1bd3f8068e83437ee8e147f440817ef986346220
Yauheni Asheichyk <yauheni.asheichyk@gmail.com>
2022-12-04 01:48:07 +0300

    Delete src directory

--- src/components/DescriptionCard.js
+++
@@ -1,9 +0,0 @@
-export default function DescriptionCard(props) {
-    return (
-        <div className="description__card">
-            <img className="description__card-image" src={props.image} alt="#" />
-            <h3 className="section__subtitle description__subtitle">{props.subtitle}</h3>
-            <p className="section__text description__text">{props.text}</p>
-        </div>
-    )
-}
